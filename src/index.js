34fcb00e8c7074c1207d5953b45675eaabbc1479
Soraire <114581621+Soraire@users.noreply.github.com>
2023-04-19 08:59:12 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import { get2Pizzas } from './services/pizzaService.js';
 import express from "express"
+import pizza from "models/"
 const app = express()
 const port = 3000
+app.use(express.json());
+
+app.put('/api/:id', (req, res)=>{
+  const id = req.params.id;
+  console.log(id);
+  const pizza = new Pizza();
+
+  pizza.nombre = req.body.Nombre;
+  pizza.precio = req.body.Precio;
+
+  console.log(pizza);
+  res.send('Hola Mundo!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,4 +23,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-await get2Pizzas();     
