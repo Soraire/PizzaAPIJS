662734b5e31b46c633a6796259242e7a9a9eba7c
Juan Ferguson <105530043+siathers@users.noreply.github.com>
2023-04-24 15:35:24 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -23,3 +23,68 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.Put("/{id}", (req, res) =>
+{
+    let respuesta = null;
+    let intRowsAffected;
+    let id = parseInt(req.params.id);
+    let pizza = req.body;
+    if (pizza.Id != id)
+    {
+        respuesta = BadRequest();
+    }
+    else
+    {
+        intRowsAffected = BD.ActualizarPizza(id, pizza);
+        if (intRowsAffected > 0)
+        {
+            respuesta = Ok(pizza);
+        }
+        else
+        {
+            respuesta = NotFound();
+        }
+    }
+    return respuesta;
+});
+
+app.Delete("/{id}", (req, res) =>
+{
+    let intRowsAffected;
+    let respuesta = null;
+    let id = parseInt(req.params.id);
+    if (id <= 0)
+    {
+        respuesta = BadRequest();
+    }
+    else
+    {
+        intRowsAffected = BD.BorrarPizzas(id);
+        if (intRowsAffected > 0)
+        {
+            respuesta = Ok();
+        }
+        else
+        {
+            respuesta = NotFound();
+        }
+    }
+    return respuesta;
+});
+
+app.Post("/", (req, res) =>
+{
+    let pizza = req.body;
+    BD.CrearPizzas(pizza);
+    return Ok(pizza);
+});
+
+BD.BuscarPizzaPorId(id)
+  .then(pizza => {
+    const pizzaInfo = document.getElementById('pizza-info');
+    pizzaInfo.innerHTML = `Nombre: ${pizza.nombre} <br> Precio: ${pizza.precio} <br> Tamaño: ${pizza.tamaño}`;
+  })
+  .catch(error => {
+    console.log('Error al buscar la pizza: ', error);
+  });
